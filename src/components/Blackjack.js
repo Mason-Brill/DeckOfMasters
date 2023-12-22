cec0b88dc0476f16953775558a9b53cb5db1dbd9
Mason Brill <masonjamesbrill@yahoo.com>
2023-12-21 20:25:17 -0500

    added stand option, need to add where dealer keeps drawing until 16 or bust

--- src/components/Blackjack.js
+++ src/components/Blackjack.js
@@ -1,22 +1,35 @@
 import {React, useState} from "react";
 
 export default function Blackjack(){
+    //the following keeps track of players choices
     const[first, changeFirst] = useState(true)
     const[hitstand, changeHitStand] = useState(false)
     const[hit, changeHit] = useState(false)
+    const[standing, changeStanding] = useState(false)
+    const[winner, changeWinner] = useState("None")
+    const [switcher, changeSwitcher] = useState(true)
+
+    //the following are to keep track of the players cards value
+    const[playersOne, changePlayersOne] = useState(0)
+    const[playersTwo, changePlayersTwo] = useState(0)
+    const[playersThird, changePlayersThird] = useState(0)
+
+    //the following are to keep track of the dealers cards value
+    const[dealersOne, changeDealersOne] = useState(0)
+    const[dealersTwo, changeDealersTwo] = useState(0)
+
+    //the following are used to keep track of the image associated with the players cards value and suit
     const[playerCard1, changePlayerCard1] = useState(0)
     const[playerCard2, changePlayerCard2] = useState(0)
     const[playerCard3, changePlayerCard3] = useState(0)
+
+    //the following are used to keep track of the image associated with the dealers cards value and suit
     const[dealerCard1, changedealerCard1] = useState(0)
     const[dealerCard2, changedealerCard2] = useState(0)
-    const [switcher, changeSwitcher] = useState(true)
 
     //two random numbers generated to be used for name of card image i.e. 312.png
     let number1 = 0
     let number2 = 0
-    let playersFirst = 0;
-    let playersSecond = 0;
-    let playersThird = 0;
 
     function Start() {
         changeFirst(false)
@@ -44,7 +57,7 @@ export default function Blackjack(){
         }
 
         //getting the value of the players first card(0-12), 'number2' is just to give the card a random suit
-        playersFirst = number1;
+        changePlayersOne(number1);
         changePlayerCard1(number1+number2);
 
         //generate random number between 0-12
@@ -59,8 +72,23 @@ export default function Blackjack(){
         }
 
         //getting the value of the players second card(0-12), 'number2' is just to give the card a random suit
-        playersSecond = number1;
+        changePlayersTwo(number1);
         changePlayerCard2(number1+number2);
+
+        //generate random number between 0-12
+        number1 = Math.floor(Math.random() * 13);
+        //generate random number between 0-3 and times it by 100
+        number2 = Math.floor(Math.random() * 4);
+        if(number2 === 0){
+            number2 = number2 + 100;
+        }
+        else {
+            number2 = number2 * 100
+        }
+
+        //getting the value of the players second card(0-12), 'number2' is just to give the card a random suit
+        changeDealersOne(number1);
+        changedealerCard1(number1+number2);
     }
 
     function choiceHit()
@@ -80,8 +108,44 @@ export default function Blackjack(){
         }
 
         //getting the value of the players second card(0-12), 'number2' is just to give the card a random suit
-        playersThird = number1;
+        changePlayersThird(number1);
         changePlayerCard3(number1+number2);
+    }
+
+    function Stand()
+    {
+        changeHitStand(false)
+        changeStanding(true)
+        //generate random number between 0-12
+        number1 = Math.floor(Math.random() * 13);
+        //generate random number between 0-3 and times it by 100
+        number2 = Math.floor(Math.random() * 4);
+        if(number2 === 0){
+            number2 = number2 + 100;
+        }
+        else {
+            number2 = number2 * 100
+        }
+
+        //getting the value of the dealers second card(0-12), 'number2' is just to give the card a random suit
+        changeDealersTwo(number1);
+        //changing the image of the dealers second card
+        changedealerCard2(number1+number2);
+
+        //dealer win?
+        if((playersOne + playersTwo) < (dealersOne + dealersTwo))
+        {
+            changeWinner("Dealer")
+        }
+        //player win?
+        else if((playersOne + playersTwo) > (dealersOne + dealersTwo))
+        {
+            changeWinner("Player")
+        }
+        //no-one wins :(
+        else{
+            changeWinner("No-One")
+        }
     }
 
     return(
@@ -124,10 +188,10 @@ export default function Blackjack(){
                         <div className="player-container">
                             <h1 className="war-title">Dealers Cards:</h1>
                             <div className="player1-cards">
-                                <img className="war-card1" src="./cards/back.png" alt="card1"/>
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${dealerCard1}.png`} alt="card1"/>
                                 <img className="war-card1" src="./cards/back.png" alt="card2"/>
                             </div>
-                            <button className="play-btn" onClick={Start}>Stand?</button>
+                            <button className="play-btn" onClick={Stand}>Stand?</button>
                         </div>
                     </div>
                 </>
@@ -149,12 +213,35 @@ export default function Blackjack(){
                         <div className="player-container">
                             <h1 className="war-title">Dealers Cards:</h1>
                             <div className="player1-cards">
-                                <img className="war-card1" src="./cards/back.png" alt="card1"/>
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${dealerCard1}.png`} alt="card1"/>
                                 <img className="war-card1" src="./cards/back.png" alt="card2"/>
                             </div>
-                            <button className="play-btn" onClick={Start}>Stand?</button>
+                            <button className="play-btn" onClick={Stand}>Stand?</button>
                         </div>
                     </div>
+                </>
+                }
+
+                {standing &&
+                <>
+                    <h1 className="war-title">Blackjack</h1>
+                    <div className="game-container">
+                        <div className="player-container">
+                            <h1 className="war-title">Your Cards:</h1>
+                            <div className="player1-cards">
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${playerCard1}.png`} alt="card1"/>
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${playerCard2}.png`} alt="card2"/>
+                            </div>
+                        </div>
+                        <div className="player-container">
+                            <h1 className="war-title">Dealers Cards:</h1>
+                            <div className="player1-cards">
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${dealerCard1}.png`} alt="card1"/>
+                                <img className={`${switcher ? "war-card1" : "war-card2"}`} src={`./cards/${dealerCard2}.png`} alt="card2"/>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 className={`${switcher ? "war-title" : "war-title"}`}>{winner} Wins!</h1>
                 </>
                 }
             </>
